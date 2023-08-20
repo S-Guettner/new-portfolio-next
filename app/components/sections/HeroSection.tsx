2e92f80f3367f5dbe2968c123962a913a571339a
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-08-20 22:19:42 +0200

    test class on mobile

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ interface HeroSectionProps {
 
 const HeroSection: FC<HeroSectionProps> = ({ }) => {
     return (
-        <main className='h-screen flex items-center justify-center'>
+        <main className='h-vh100 flex items-center justify-center'>
             <h1 className='text-white'>HEROSECTION</h1>
         </main>
     )
--- app/components/sections/SkillsSection.tsx
+++ app/components/sections/SkillsSection.tsx
@@ -6,7 +6,7 @@ interface SkillsSectionProps {
 
 const SkillsSection: FC<SkillsSectionProps> = ({}) => {
   return( 
-      <main className='h-screen flex items-center justify-center'>
+      <main className='h-vh100 flex items-center justify-center'>
           <h1 className='text-white'>SKILLSSECTION</h1>
       </main>
 )
