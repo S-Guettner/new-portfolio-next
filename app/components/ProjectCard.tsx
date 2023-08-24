3bddf9f7d0075c92255532d1d080300c387d457a
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-08-24 14:54:05 +0200

    added image to about

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -44,8 +44,33 @@ const ProjectCard: FC<ProjectCardProps> = ({ currentCard }) => {
     }
     else if (currentCard === 1) {
         return (
-            <main className='h-60 bg-white bg-opacity-10 rounded-xl shadow-2xl backdrop-blur-md p-4'>
-                <p className='text-textLarge'>HELLLOO 222222222222</p>
+            <main className='justify-between pb-8 bg-white bg-opacity-10 rounded-xl shadow-2xl backdrop-blur-md p-4'>
+                <h2 className='text-textLarge mb-2 text-2xl'>My Portfolio</h2>
+                <p className='text-text mb-2'>Welcome to my front-end developer portfolio. Explore my journey, skills, and the passion I bring to every piece of code.</p>
+                <section className='my-5'>
+                    <section className='flex justify-evenly '>
+                        <div className=''>
+                            <Image
+                                src={codeCareer}
+                                width={100}
+                                height={100}
+                                alt='Logo Code-career'
+                            />
+                        </div>
+                        <div className='pt-10'>
+                            <p className='text-green '>&lt;Stack&gt;</p>
+                            <p className='pl-6 text-textLarge'>- Next.js</p>
+                            <p className='pl-6 text-textLarge'>- gsap</p>
+                            <p className='pl-6 text-textLarge'>- Tailwind CSS ❤️</p>
+                            <p className='text-green mb-8'>&lt;/Stack&gt;</p>
+                        </div>
+                    </section>
+                </section>
+
+
+                <Link href={"https://code-career.vercel.app/"}>
+                    <p className='text-green resume-button inline border border-green p-2  rounded'>Visit the Project</p>
+                </Link>
             </main>
         )
     }
--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { FC, useState } from 'react'
 import Navbar from '../navigation/Navbar'
+import Image from 'next/image'
+import portrait from '../../../public/portraitMe.png'
 
 
 interface HeroSectionProps {
@@ -21,18 +23,28 @@ const HeroSection: FC<HeroSectionProps> = ({ currentSection, totalSections, onSe
                 onSectionChange={onSectionChange}
             />
 
-            <section className='p-4 pb-0 mb-4 pt-20'>
+            <section className='p-4 pb-0 mb-4 pt-10'>
                 <p className='text-green mb-2'>&lt;About&gt;</p>
                 <p className=' text-green pl-4 mb-1'>Hi, my name is</p>
                 <h1 className='text-textLarge text-3xl mb-1 font-bold pl-4'><span className='text-green'>&lt;</span>Sven Güttner <span className='text-green'>/&gt;</span> </h1>
-                <p className='text-green pl-4'>&lt;p&gt;</p>
+                <p className='text-green pl-6'>&lt;p&gt;</p>
                 <div className='text-text'>
                     <p className='pl-8'>I am a web developer who loves to tackle new challenges and is constantly on a learning journey. My goal is to not just build websites, but to create experiences that resonate with users.</p>
                 </div>
                 <p className='text-green pl-6 '>&lt;/p&gt;</p>
+                <p className='text-green pl-6'>&lt;Image&gt;</p>
+                <section className='flex justify-center'>
+                    <Image
+                        src={portrait}
+                        height={270}
+                        width={270}
+                        alt='image of me'
+                    />
+                </section>
+                <p className='text-green pl-6'>&lt;/Image&gt;</p>
                 <p className='text-green'>&lt;/About&gt;</p>
             </section>
-           
+
         </main>
     )
 }
